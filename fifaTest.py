0928adcee5f73d3048dd54832b067737f9e4ef69
Manish200 <mrai8450@gmail.com>
2017-11-09 23:50:33 +0530

    adding plots

--- fifaTest.py
+++ fifaTest.py
@@ -1,20 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  9 23:32:24 2017
+
+@author: Dell
+"""
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pylab as pl
 
 data=pd.read_csv('FullData.csv')
 
-name=data['Name'];
-
-
+xTicks=data['Name'];
 
 ws=data.loc[:,('Freekick_Accuracy' , 'Volleys' , 'Curve' , 'Long_Shots' , 'Finishing' , 'Shot_Power' , 'Ball_Control' , 'Marking' ,
 	           'Attacking_Position' , 'Vision' , 'Acceleration' , 'Strength' , 'Balance' , 'Agility' , 'Heading')]
 
-
-a=ws.mean(axis=1);
-
-b=a.tolist()
-nme =name.tolist()
-plt.plot(nme,b);
-plt.show()
+y=ws.mean(axis=1);
+x = list(range(17588))
+pl.xticks(x, xTicks)
+pl.xticks(range(2), xTicks, rotation=45) #writes strings with 45 degree angle
+pl.title('best goal')
+pl.xlabel('players')
+pl.ylabel('total goals')
+pl.plot(x,y)
+pl.show()
